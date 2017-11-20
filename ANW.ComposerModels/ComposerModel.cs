8b77221b2829ac46ee35c0544e16421db233effd
D0tNetGeek <vmalvi2003@yahoo.com>
2017-11-20 00:28:12 +0000

    Models and solution file

---
+++ ANW.ComposerModels/ComposerModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ANW.ComposerModels
+{
+    public class ComposerModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string FullName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Awards { get; set; }
+    }
+}
